0167d0bebf3585e4d76c6a8874f6e5dd01a11c7e
Astrall <jar.mus@o2.pl>
2022-10-12 08:28:16 +0200

    zmiany

---
+++ php/1_db/1_select.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Użytkownicy</title>
+  </head>
+  <body>
+    <h3>Użytkownicy z tabeli users</h3>
+  </body>
+</html>
